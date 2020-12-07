622e8f87c6b0d9de344ec4ddc2b88acc7cd39247
Adrianopvh <adriano.rodriques12389@gmail.com>
2020-12-07 18:39:50 -0400

    resolver zoom do price

--- scripts.js
+++ scripts.js
@@ -10,7 +10,6 @@ for (let card of cards) {
     })
 }
 
-
 document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove("active")
     modalOverlay.querySelector("iframe").src = ""
